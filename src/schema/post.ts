b044abeaea8ffa8d056ef749b8ffb65f28d083fd
Gabriel Turcheti <gturcheti@MacBook-Air-de-Gabriel.local>
2025-03-24 16:10:28 -0300

    feat: schemas

--- src/schema/post.ts
+++ src/schema/post.ts
@@ -1,3 +1,7 @@
-import yup from 'yup'
+import * as Yup from 'yup';
 
-export const postSchema = yup.object()
+export const postSchema = Yup.object().shape({
+  author: Yup.number().required('Por favor, informe o autor'),
+  title: Yup.string().required('Por favor, informe um título'),
+  content: Yup.string().required('Por favor, preencha o conteúdo do post'),
+});
